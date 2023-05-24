c8eee7135566b8d8c3dcbc94d3d885f6cfddd28d
songseungkwan <ssk3553@Naver.com>
2023-05-24 16:32:20 +0900

    dsadas

---
+++ src/com/mycompany/secondJava/ch02/HelloSong.java
@@ -0,0 +1,10 @@
+package com.mycompany.secondJava.ch02;
+
+public class HelloSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello song`s world");
+	}
+
+}
